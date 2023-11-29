f97401f9645b5f2f3511ac6b205bb2b8d27f105c
ewuerger <ernst.wuerger@gmail.com>
2023-11-29 11:46:23 +0100

    docs: Fix `create_grouped_link_fields` docstring

--- capella2polarion/elements/element.py
+++ capella2polarion/elements/element.py
@@ -208,14 +208,15 @@ def create_grouped_link_fields(
     work_item: serialize.CapellaWorkItem,
     back_links: dict[str, list[polarion_api.WorkItemLink]] | None = None,
 ):
-    """Create the grouped link workitems fields from of the primary work items.
+    """Create the grouped link work items fields from the primary work item.
 
     Parameters
     ----------
     work_item
-        WorkItem to create the fields for
+        WorkItem to create the fields for.
     back_links
-        A dictionary of secondary WorkItem IDs to links to create backlinks later
+        A dictionary of secondary WorkItem IDs to links to create
+        backlinks later.
     """
     wi = f"[{work_item.id}]({work_item.type} {work_item.title})"
     logger.debug("Building grouped links for work item %r...", wi)
