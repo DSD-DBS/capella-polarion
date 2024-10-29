7d9c0c6ee8e8c1a592aee144efaea747decf99d2
micha91 <michael.harbarth@deutschebahn.com>
2024-10-29 17:15:31 +0100

    feat: Generate WorkItems only once (#123)

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -137,8 +137,8 @@ def test_migrate_model_elements(cli_mocks: CLIMocks):
 
     assert result.exit_code == 0
     assert cli_mocks.get_polarion_wi_map.call_count == 1
-    assert cli_mocks.generate_work_items.call_count == 2
-    assert cli_mocks.generate_work_items.call_args_list[1][1] == {
+    assert cli_mocks.generate_work_items.call_count == 1
+    assert cli_mocks.generate_work_items.call_args_list[0][1] == {
         "generate_links": True,
         "generate_attachments": True,
         "generate_grouped_links_custom_fields": True,
