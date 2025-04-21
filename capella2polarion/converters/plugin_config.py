6578491d17592acb3bdc374c29c8d510d39d26c5
Michael Harbarth <michael.harbarth@deutschebahn.com>
2025-04-21 17:20:47 +0200

    feat: Implement first plugin interfaces - WIP

---
+++ capella2polarion/converters/plugin_config.py
@@ -0,0 +1,21 @@
+# Copyright DB InfraGO AG and contributors
+# SPDX-License-Identifier: Apache-2.0
+"""Plugin configuration classes and functions."""
+import typing as t
+
+import pydantic
+import yaml
+
+
+class PluginConfig(pydantic.BaseModel):
+    """Configuration for a plugin config."""
+
+    module: str
+    plugin_name: str
+    args: dict[str, t.Any]
+
+
+def read_config_file(config: t.TextIO) -> list[PluginConfig]:
+    """Read a yaml containing a list of PluginConfigs."""
+    config_content = yaml.safe_load(config)
+    return [PluginConfig(**config_item) for config_item in config_content]
